7b8793526f6f3eae5a6296f0550cdf945e72ccfb
João Pedro Alves <joaopealves.dev@gmail.com>
2024-10-28 11:00:35 -0300

    chore: improve db script

--- priv/init-db.sh
+++ priv/init-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Run the standard entrypoint setup from Postgres
-export PGPASSWORD="postgres"
+export PGPASSWORD="$POSTGRES_PASSWORD"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USERNAME" <<-EOSQL
     CREATE DATABASE plotwist_dev;
