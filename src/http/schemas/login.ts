6aded2a6f7e8e1b14fa7250cc47e16a092597146
Luiz Henrique <7henrique18@gmail.com>
2024-10-31 11:37:04 -0300

    feat: response schemas (#13)
    * feat: response schemas
    
    * feat: changelog

--- src/http/schemas/login.ts
+++ src/http/schemas/login.ts
@@ -7,3 +7,14 @@ export const loginBodySchema = z.object({
     .min(8, 'Password must be at least 8 characters long')
     .default('password123'),
 })
+
+export const loginResponseSchema = {
+  200: z.object({
+    token: z.string(),
+  }),
+  400: z
+    .object({
+      message: z.string(),
+    })
+    .describe('Invalid email or password.'),
+}
--- src/http/schemas/users.ts
+++ src/http/schemas/users.ts
@@ -1,3 +1,5 @@
+import { schema } from '@/db/schema'
+import { createInsertSchema } from 'drizzle-zod'
 import { z } from 'zod'
 
 export const createUserBodySchema = z.object({
@@ -6,10 +8,50 @@ export const createUserBodySchema = z.object({
   password: z.string().min(8, 'Password must be at least 8 characters long'),
 })
 
+export const createUserResponseSchema = {
+  409: z
+    .object({
+      message: z.string(),
+    })
+    .describe('Email or username is already registered.'),
+  500: z
+    .object({
+      message: z.string(),
+    })
+    .describe('Fail to hash password.'),
+  200: z
+    .object({
+      user: createInsertSchema(schema.users).omit({ password: true }),
+    })
+    .describe('Fail to hash password.'),
+}
+
 export const checkAvailableUsernameQuerySchema = z.object({
   username: z.string().min(1, 'Username is required.'),
 })
 
+export const checkAvailableUsernameResponseSchema = {
+  200: z.object({
+    available: z.boolean(),
+  }),
+  409: z
+    .object({
+      message: z.string(),
+    })
+    .describe('Username is already registered.'),
+}
+
 export const isEmailAvailableQuerySchema = z.object({
   email: z.string().email('Email is invalid.'),
 })
+
+export const isEmailAvailableResponseSchema = {
+  200: z.object({
+    available: z.boolean(),
+  }),
+  409: z
+    .object({
+      message: z.string(),
+    })
+    .describe('Email is already registered.'),
+}
