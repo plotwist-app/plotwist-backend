f084f3b8b2dcc46971212647f2cbbffd1021d706
João Pedro Alves <joaopealves.dev@gmail.com>
2024-11-04 12:06:02 -0300

    fix: invalid path

--- src/app/domain/services/lists/get-lists.spec.ts
+++ src/app/domain/services/lists/get-lists.spec.ts
@@ -3,8 +3,6 @@ import { describe, expect, it } from 'vitest'
 import { makeUser } from '@/test/factories/make-user'
 import { makeList } from '@/test/factories/make-list'
 import { getLists } from './get-lists'
-import { UserNotFoundError } from '../../errors/user-not-found'
-import { faker } from '@faker-js/faker'
 
 describe('get lists', () => {
   it('should be able to get lists by user id ', async () => {
@@ -21,11 +19,5 @@ describe('get lists', () => {
         expect.objectContaining({ id: secondList.id }),
       ]),
     })
-  })
-
-  it('should not be able to get lists by invalid user id', async () => {
-    const sut = await getLists({ userId: faker.string.uuid() })
-
-    expect(sut).toBeInstanceOf(UserNotFoundError)
   })
 })
--- src/app/domain/services/reviews/create-review.ts
+++ src/app/domain/services/reviews/create-review.ts
@@ -1,7 +1,7 @@
 import { insertReview } from '@/db/repositories/reviews-repository'
 import type { InsertReviewModel } from '../../entities/review'
 import { UserNotFoundError } from '../../errors/user-not-found'
-import { getUserById } from '../lists/users/get-by-id'
+import { getUserById } from '../users/get-by-id'
 
 export async function createReview(params: InsertReviewModel) {
   const result = await getUserById(params.userId)
