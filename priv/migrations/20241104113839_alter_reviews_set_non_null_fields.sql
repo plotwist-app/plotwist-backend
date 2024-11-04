60c2629b4a892cf17e25e195245f778466ff8ca0
João Pedro Alves <joaopealves.dev@gmail.com>
2024-11-04 12:01:09 -0300

    chore: fix migrations merge conflict

--- priv/migrations/20241104113839_alter_reviews_set_non_null_fields.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "reviews" ALTER COLUMN "review" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "reviews" ALTER COLUMN "rating" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "reviews" ALTER COLUMN "has_spoilers" SET DEFAULT false;
