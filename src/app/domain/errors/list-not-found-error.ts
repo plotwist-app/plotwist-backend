08cd2059900fcc424472e2925ecdb8594babb45e
Luiz Henrique <7henrique18@gmail.com>
2024-11-04 13:21:30 -0300

    fix: status code comment

--- src/app/domain/errors/list-not-found-error.ts
+++ src/app/domain/errors/list-not-found-error.ts
@@ -2,6 +2,6 @@ import { DomainError } from './domain-error'
 
 export class ListNotFoundError extends DomainError {
   constructor(message?: string) {
-    super(message ?? 'List not found.', 404) // Unauthorized
+    super(message ?? 'List not found.', 404) // Not found
   }
 }
