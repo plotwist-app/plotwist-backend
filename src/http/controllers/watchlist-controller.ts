fa1dbe279933a52de2eab3fe6835df11ae5fcb30
lui7henrique <7henrique18@gmail.com>
2024-11-10 16:18:08 -0300

    fix: types

--- src/http/controllers/watchlist-controller.ts
+++ src/http/controllers/watchlist-controller.ts
@@ -1,6 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { createWatchlistItemBodySchema } from '../schemas/watchlist'
 import { createWatchlistItemService } from '@/domain/services/watchlist-item/create-watchlist-item'
+import { DomainError } from '@/domain/errors/domain-error'
 
 export async function createWatchlistItemController(
   request: FastifyRequest,
@@ -10,11 +11,15 @@ export async function createWatchlistItemController(
     request.body
   )
 
-  const { watchlistItem } = await createWatchlistItemService({
+  const result = await createWatchlistItemService({
     tmdbId,
     mediaType,
     userId: request.user.id,
   })
 
-  return reply.status(201).send({ watchlistItem })
+  if (result instanceof DomainError) {
+    return reply.status(result.status).send({ message: result.message })
+  }
+
+  return reply.status(201).send({ result })
 }
