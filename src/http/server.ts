5ce1bdf70037eba5fa0a9c0371d141bf08689cc6
Luiz Henrique <7henrique18@gmail.com>
2024-11-12 16:39:10 -0300

    fix; remove log

--- src/http/server.ts
+++ src/http/server.ts
@@ -50,7 +50,6 @@ app.register(fastifySwagger, {
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
-    console.log({ error })
     return reply
       .status(400)
       .send({ message: 'Validation error.', issues: error.format() })
