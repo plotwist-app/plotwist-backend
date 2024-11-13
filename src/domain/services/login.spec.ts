5edb2813ed32e0e111d2d19f0a5a2c1805a48aaf
Luiz Henrique <7henrique18@gmail.com>
2024-11-13 15:43:06 -0300

    fix: test

--- src/domain/services/login.spec.ts
+++ src/domain/services/login.spec.ts
@@ -6,7 +6,7 @@ import { faker } from '@faker-js/faker'
 
 import { InvalidEmailError } from '../errors/invalid-email-error'
 import { InvalidPasswordError } from '../errors/invalid-password-error'
-import { login } from './login'
+import { loginService } from './login'
 
 describe('login', () => {
   it('should be able to login', async () => {
@@ -14,7 +14,7 @@ describe('login', () => {
     const hashedPassword = await hashPassword(password)
 
     const user = await makeUser({ password: hashedPassword })
-    const sut = await login({ email: user.email, password })
+    const sut = await loginService({ email: user.email, password })
 
     expect(sut).toBeTruthy()
     expect(sut).toEqual({
@@ -26,14 +26,20 @@ describe('login', () => {
 
   it('should not be able to login with non-existent user', async () => {
     const user = makeRawUser()
-    const sut = await login({ email: user.email, password: user.password })
+    const sut = await loginService({
+      email: user.email,
+      password: user.password,
+    })
 
     expect(sut).toBeInstanceOf(InvalidEmailError)
   })
 
   it('should not be able to login with invalid credentials', async () => {
     const user = await makeUser()
-    const sut = await login({ email: user.email, password: user.password })
+    const sut = await loginService({
+      email: user.email,
+      password: user.password,
+    })
 
     expect(sut).toBeInstanceOf(InvalidPasswordError)
   })
