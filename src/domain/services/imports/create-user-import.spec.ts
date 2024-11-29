4dea90d70361830814380e82d8897e542b631e06
João Pedro Alves <joaopealves.dev@gmail.com>
2024-11-28 22:57:59 -0300

    chore: update test

--- src/domain/services/imports/create-user-import.spec.ts
+++ src/domain/services/imports/create-user-import.spec.ts
@@ -5,10 +5,11 @@ import { createUserImport } from './create-user-import'
 import { makeUser } from '@/test/factories/make-user'
 
 import type { User } from '@/domain/entities/user'
+import { makeUserEpisode } from '@/test/factories/make-user-import'
 
 describe('create like', () => {
   it('should be able to create an user import', async () => {
-    const sut = createUserImport({})
+    const sut = makeUserEpisode({})
 
     expect(sut).toEqual({
       like: expect.objectContaining(true),
