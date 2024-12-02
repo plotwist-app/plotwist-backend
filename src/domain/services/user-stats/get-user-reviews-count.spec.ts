50212083ff5a21cea85080a1581e10ef8d243260
Luiz Henrique <7henrique18@gmail.com>
2024-12-02 09:40:01 -0300

    test: get user reviews count

---
+++ src/domain/services/user-stats/get-user-reviews-count.spec.ts
@@ -0,0 +1,19 @@
+import { makeUser } from '@/test/factories/make-user'
+import { makeReview } from '@/test/factories/make-review'
+import { getUserReviewsCountService } from './get-user-reviews-count'
+
+describe('get user reviews count', () => {
+  it('should be able to get reviews count', async () => {
+    const user = await makeUser()
+
+    await makeReview({ userId: user.id })
+    await makeReview({ userId: user.id })
+    await makeReview({ userId: user.id })
+
+    const sut = await getUserReviewsCountService(user.id)
+
+    expect(sut).toEqual({
+      reviewsCount: 3,
+    })
+  })
+})
