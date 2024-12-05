3626dc30ad1f7635ef2758b23f35541d89f3741e
Luiz Henrique <7henrique18@gmail.com>
2024-12-05 18:52:26 -0300

    fix: remove unused

--- src/utils/image.ts
+++
@@ -1,7 +0,0 @@
-import sharp from 'sharp'
-
-export async function convertToWebp(buffer: Buffer) {
-  return sharp(buffer)
-    .webp({ quality: 80 }) // Define a qualidade da imagem .webp
-    .toBuffer()
-}
