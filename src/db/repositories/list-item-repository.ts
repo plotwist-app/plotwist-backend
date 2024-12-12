6009275880764830755736e23986a936547e9762
Luiz Henrique <7henrique18@gmail.com>
2024-12-12 15:11:28 -0300

    Task/plo 113 (#61)
    * feat: update list item position
    
    * feat: update list items
    
    * feat: update list item transaction
    
    * fix: remove package manager
    
    * fix: remove unnecessary console log in updateListItemsService
    
    * feat: changelog

--- src/db/repositories/list-item-repository.ts
+++ src/db/repositories/list-item-repository.ts
@@ -2,6 +2,7 @@ import type { InsertListItem } from '@/domain/entities/list-item'
 import { eq } from 'drizzle-orm'
 import { db } from '..'
 import { schema } from '../schema'
+import type { UpdateListItemsServiceInput } from '@/domain/services/list-item/update-list-items'
 
 export async function insertListItem(input: InsertListItem) {
   return db.insert(schema.listItems).values(input).returning()
@@ -12,7 +13,7 @@ export async function selectListItems(listId: string) {
     .select()
     .from(schema.listItems)
     .where(eq(schema.listItems.listId, listId))
-    .orderBy(schema.listItems.createdAt)
+    .orderBy(schema.listItems.position)
 }
 
 export async function deleteListItem(id: string) {
@@ -21,4 +22,20 @@ export async function deleteListItem(id: string) {
 
 export async function getListItem(id: string) {
   return db.select().from(schema.listItems).where(eq(schema.listItems.id, id))
+}
+
+export async function updateListItems({
+  listItems,
+}: UpdateListItemsServiceInput) {
+  return db.transaction(async tx => {
+    const promises = listItems.map(({ id, position }) =>
+      tx
+        .update(schema.listItems)
+        .set({ position })
+        .where(eq(schema.listItems.id, id))
+        .returning()
+    )
+
+    return await Promise.all(promises)
+  })
 }
