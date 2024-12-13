f835532216136c6d83279a0d85d74376d95b6128
João Pedro Alves <joaopealves.dev@gmail.com>
2024-12-13 10:00:16 -0300

    WIP: import MAL dispatcher

--- src/domain/services/imports/decoder/decode-my-anime-list.ts
+++ src/domain/services/imports/decoder/decode-my-anime-list.ts
@@ -1,5 +1,13 @@
+import { InsertUserImportWithItems, UserImport } from '@/domain/entities/import'
+import { InsertSeriesImportWithoutImportId } from '@/domain/entities/import-series'
 import { unzipFile } from '@/domain/helpers/unzip-file'
 import { convertXmlToJson } from '@/domain/helpers/xml-to-json'
+import {
+  MALAnimes,
+  MalStatus,
+  MyAnimeListImport,
+  SeriesType,
+} from '@/domain/value_objects/my-anime-list-import'
 import type { MultipartFile } from '@fastify/multipart'
 
 export async function decodeMyAnimeList(
@@ -8,10 +16,52 @@ export async function decodeMyAnimeList(
 ) {
   try {
     const unzippedContent = await unzipFile(uploadedFile)
-    const parsedFile = convertXmlToJson(unzippedContent)
+    const parsedFile: MyAnimeListImport = convertXmlToJson(unzippedContent)
+
+    const { movies: rawMovies, series: rawSeries } = separateByType(
+      parsedFile.myanimelist.anime
+    )
+
+    console.log(rawMovies)
+    // const userImport: InsertUserImportWithItems = {
+    //   importStatus: 'NOT_STARTED',
+    //   itemsCount: items.length,
+    //   provider: 'MY_ANIME_LIST',
+    //   userId,
+    // }
 
     return
   } catch (error) {
     return error
+  }
+}
+
+function separateByType(mediaArray: MALAnimes[]) {
+  const series = mediaArray.filter(
+    item => item.series_type === SeriesType.SERIES
+  )
+  const movies = mediaArray.filter(
+    item => item.series_type === SeriesType.MOVIE
+  )
+
+  return { series, movies }
+}
+
+function buildSeries(rawSeries: MALAnimes[]) {
+  const series = rawSeries.map(item => {
+    const userImport: InsertSeriesImportWithoutImportId = {
+      importStatus: 'NOT_STARTED',
+      name: item.series_title,
+      endDate: item.my_finish_date ?? null,
+      startDate: item.my_start_date ?? null,
+      watchedEpisodes: item.my_watched_episodes ?? null,
+      userItemStatus: item.my_status,
+    }
+  })
+}
+
+function convertStatus(status: MalStatus) {
+  if (status === MalStatus.COMPLETED) {
+    return 'COMPLETED'
   }
 }
---
+++ src/domain/value_objects/my-anime-list-import.ts
@@ -0,0 +1,55 @@
+export type MyAnimeListImport = {
+  '?xml': xmtMetadata
+  myanimelist: {
+    userInfo: UserInfo
+    anime: MALAnimes[]
+  }
+}
+
+type UserInfo = {
+  user_name: string
+  user_export_type: number
+  user_total_anime: number
+  user_total_watching: number
+  user_total_completed: number
+  user_total_onhold: number
+  user_total_dropped: number
+  user_total_plantowatch: number
+}
+
+export enum MalStatus {
+  COMPLETED = 'Completed',
+}
+
+export enum SeriesType {
+  MOVIE = 'Movie',
+  SERIES = 'Tv',
+}
+
+export type MALAnimes = {
+  series_animedb_id: number
+  series_title: string
+  series_type: SeriesType
+  series_episodes: number
+  my_id: number
+  my_watched_episodes: number
+  my_start_date: Date
+  my_finish_date: Date
+  my_rated: string
+  my_score: number
+  my_storage: string
+  my_storage_value: number
+  my_status: MalStatus
+  my_comments: string
+  my_times_watched: number
+  my_rewatch_value: string
+  my_priority: string
+  my_tags: string
+  my_rewatching: number
+  my_rewatching_ep: number
+  my_discuss: number
+  my_sns: string
+  update_on_import: number
+}
+
+type xmtMetadata = { '@_version': string; '@_encoding': string }
