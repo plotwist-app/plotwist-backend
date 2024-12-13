31c1e2d7b78d1af619c72c6968218b37e6ec3db0
Luiz Henrique <7henrique18@gmail.com>
2024-12-13 20:20:33 -0300

    fix: get reviews replies without auth

--- src/domain/services/review-replies/get-review-replies.ts
+++ src/domain/services/review-replies/get-review-replies.ts
@@ -2,7 +2,7 @@ import { selectReviewReplies } from '@/db/repositories/review-replies-repository
 
 export async function getReviewRepliesService(
   reviewId: string,
-  authenticatedUserId: string
+  authenticatedUserId?: string
 ) {
   const reviewReplies = await selectReviewReplies(reviewId, authenticatedUserId)
 
