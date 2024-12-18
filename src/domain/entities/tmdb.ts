17c233cd6f5880c150747adbaf9e27f00efff1c0
Luiz Henrique <7henrique18@gmail.com>
2024-12-18 18:34:56 -0300

    feat: default http agent

--- src/domain/entities/tmdb.ts
+++ src/domain/entities/tmdb.ts
@@ -1,4 +1,7 @@
 import { config } from '@/env'
 import { TMDB } from '@plotwist_app/tmdb'
+import https from 'https'
 
-export const tmdb = TMDB(config.services.TMDB_ACCESS_TOKEN)
+export const tmdb = TMDB(config.services.TMDB_ACCESS_TOKEN, {
+  httpsAgent: new https.Agent({ family: 4 }),
+})
