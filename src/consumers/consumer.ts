b41ff6fa031366e49af8913eccb0587201fecb2e
João Pedro Alves <joaopealves.dev@gmail.com>
2024-12-22 07:22:11 -0300

    chore: add consumer.ts

--- src/consumers/consumer.ts
+++ src/consumers/consumer.ts
@@ -4,37 +4,29 @@ export async function consumeMessages(
   queueUrl: string,
   processMessage: (message: string) => Promise<void>
 ) {
-  if (!queueUrl) {
-    throw new Error('Queue URL must be a valid string')
-  }
-
   while (true) {
     try {
       const messages = await SqsAdapter.receiveMessage(queueUrl)
-      if (messages) {
-        await processMessages(
-          messages.filter((msg): msg is string => typeof msg === 'string'),
-          processMessage
-        )
+
+      if (messages && messages.length > 0) {
+        for (const message of messages) {
+          if (typeof message === 'string') {
+            try {
+              await processMessage(message)
+            } catch (error) {
+              console.error(`Error processing message: ${message}`, error)
+            }
+          } else {
+            console.warn('Received message is not a string:', message)
+          }
+        }
+      } else {
+        console.info('No messages to process.')
       }
+
       await delay(2000)
     } catch (error) {
       console.error(`Error consuming messages from queue: ${queueUrl}`, error)
-    }
-  }
-}
-
-async function processMessages(
-  messages: string[],
-  processMessage: (message: string) => Promise<void>
-): Promise<void> {
-  if (messages.length === 0) return
-
-  for (const message of messages) {
-    try {
-      await processMessage(message)
-    } catch (error) {
-      console.error(`Error processing message: ${message}`, error)
     }
   }
 }
