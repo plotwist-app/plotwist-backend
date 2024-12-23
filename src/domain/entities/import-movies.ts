0e330f0fe368e19ca71ef0d3a444231a0eab5297
João Pedro Alves <joaopealves.dev@gmail.com>
2024-12-23 13:39:07 -0300

    style: fix type check

--- src/domain/entities/import-movies.ts
+++ src/domain/entities/import-movies.ts
@@ -1,13 +1,9 @@
 import type { schema } from '@/db/schema'
 import type { InferInsertModel, InferSelectModel } from 'drizzle-orm'
 
-export type ImportMovie = InferSelectModel<typeof schema.importMovies> & {
-  __metadata: object
-}
+export type ImportMovie = InferSelectModel<typeof schema.importMovies>
 
 export type InsertImportMovie = Omit<
   InferInsertModel<typeof schema.importMovies>,
   'importId' | 'id'
-> & {
-  __metadata: object
-}
+>
