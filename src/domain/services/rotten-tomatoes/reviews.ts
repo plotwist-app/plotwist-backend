c4abaa0c5d74ebc6fdde0def9ecb5b3a6c966442
Luiz Henrique <7henrique18@gmail.com>
2025-01-19 07:43:45 -0300

    chore: update Dockerfile and package.json for Puppeteer integration with Chromium

--- src/domain/services/rotten-tomatoes/reviews.ts
+++ src/domain/services/rotten-tomatoes/reviews.ts
@@ -15,7 +15,13 @@ export async function scrapeRottenTomatoesReviews({
 }: GetRottenTomatoesReviewsInput) {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: '/usr/bin/chromium-browser',
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+    ],
   })
 
   try {
