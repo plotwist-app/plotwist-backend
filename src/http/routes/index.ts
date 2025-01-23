b151f51d207aa26572e588d3b15f98233d03b06b
João Pedro Alves <joaopealves.dev@gmail.com>
2025-01-22 21:48:44 -0300

    chore: remove func

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -63,6 +63,8 @@ export function routes(app: FastifyInstance) {
   app.register(followsRoutes)
   app.register(importRoutes)
   app.register(userActivitiesRoutes)
+
+  return
 }
 
 function getCorsOrigin() {
