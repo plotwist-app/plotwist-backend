727c1e26db172828e54e0b9b150a87ef15b8131c
Luiz Henrique <7henrique18@gmail.com>
2025-02-05 11:48:20 -0300

    chore: Remove console log from lists retrieval

--- src/http/controllers/list-controller.ts
+++ src/http/controllers/list-controller.ts
@@ -54,8 +54,6 @@ export async function getListsController(
     request.query
   )
 
-  console.log({ limit, hasBanner })
-
   const result = await getListsServices({
     userId,
     authenticatedUserId: request.user?.id,
